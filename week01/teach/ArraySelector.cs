4895d4184fc049eaca28270677ec36b8ba322939
RayVenShellhart <rayvenshellhart@gmail.com>
2026-01-07 16:26:27 -0800

    week1-midweek

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,28 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        // Creates list with 10 spaces
+        var result = new int[select.Length];
+        // tracks list items taken
+        var l1Idx = 0;
+        var l2Idx = 0;
+        // if one takes result from first list and move point forward. if two takes from second with the same process
+        for (var i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+                result[i] = list1[l1Idx++];
+            else
+                result[i] = list2[l2Idx++];
+        }
+
+        return result;
     }
 }
